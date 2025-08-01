7097cdfa80639403c4031138c69b3b596cd1aa5b
giovaneneves7 <autornekoyasha@gmail.com>
2025-08-01 15:51:52 -0300

    fisrt commit

---
+++ src/main/java/br/edu/ifba/conectairece/ApiConectaIreceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.conectairece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConectaIreceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConectaIreceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifba/conectairece/ApiConectaIreceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.conectairece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConectaIreceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

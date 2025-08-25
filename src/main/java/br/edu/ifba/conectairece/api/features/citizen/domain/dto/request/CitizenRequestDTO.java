409c5624634e37c69a085ff30c27ea493fc13f56
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2025-08-25 09:54:03 -0300

    refactor: adicionando anotações do Lombok no DTO

--- src/main/java/br/edu/ifba/conectairece/api/features/citizen/domain/dto/request/CitizenRequestDTO.java
+++ src/main/java/br/edu/ifba/conectairece/api/features/citizen/domain/dto/request/CitizenRequestDTO.java
@@ -1,6 +1,10 @@
 package br.edu.ifba.conectairece.api.features.citizen.domain.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Data Transfer Object for receiving Citizen data in API requests.
@@ -8,6 +12,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
  *
  * @author Jorge Roberto
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class CitizenRequestDTO {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String govProfileSnapshot;

fcafb0d00f779b50a699c6c94b78f577a113fe44
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2025-09-02 10:27:16 -0300

    fix: Adicionando anotações aos DTOs

--- src/main/java/br/edu/ifba/conectairece/api/features/category/domain/dto/request/CategoryRequestDto.java
+++ src/main/java/br/edu/ifba/conectairece/api/features/category/domain/dto/request/CategoryRequestDto.java
@@ -2,6 +2,9 @@ package br.edu.ifba.conectairece.api.features.category.domain.dto.request;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,16 +15,22 @@ import lombok.Setter;
  * Used when creating or updating category information, including
  * name, description, and an optional image URL.
  *
- * @author Caio Alves
+ * @author Caio Alves, Jorge Roberto
  */
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryRequestDto implements Serializable {
 
+    @JsonProperty("name")
+    @NotNull(message = "Name is mandatory.")
+    @NotBlank(message = "Name cannot be blank.")
     private String name;
+
+    @JsonProperty("description")
     private String description;
+
+    @JsonProperty("imageUrl")
     private String imageUrl;
 }

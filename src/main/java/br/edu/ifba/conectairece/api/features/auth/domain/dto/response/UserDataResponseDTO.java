7d25e68a390e77bb67d79afbe1b6163f8e7a0e3f
giovaneneves7 <autornekoyasha@gmail.com>
2025-09-07 14:38:46 -0300

    ✨ feat(auth): Create dto to user data response

--- src/main/java/br/edu/ifba/conectairece/api/features/auth/domain/dto/response/UserDataResponseDTO.java
+++ src/main/java/br/edu/ifba/conectairece/api/features/auth/domain/dto/response/UserDataResponseDTO.java
@@ -4,16 +4,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import br.edu.ifba.conectairece.api.features.auth.domain.enums.UserStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
-public record UserDataResponseDTO(
+/**
+ * @author Giovane Neves 
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDataResponseDTO{
 
 	@JsonProperty("id")
-    UUID id,
+    private UUID id;
 
 	@JsonProperty("username")
-    String username,
+    private String username;
 
     @JsonProperty("email")
-    String email
-){}
+    private String email;
+
+    @JsonProperty("userStatus")
+    private UserStatus userStatus;
+}

d6abeff2bbb7acb45b562346f18042569aac5f04
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2025-10-01 13:56:58 -0300

    :zap: feat(CI-99) : create admin domain

---
+++ src/main/java/br/edu/ifba/conectairece/api/features/admin/domain/service/AdminService.java
@@ -0,0 +1,10 @@
+package br.edu.ifba.conectairece.api.features.admin.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService implements IAdminService{
+
+}

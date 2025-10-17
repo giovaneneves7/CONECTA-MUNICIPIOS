287e195b770a3c61c6609594f834d4b9c19509b7
giovaneneves7 <autornekoyasha@gmail.com>
2025-10-17 09:12:14 -0300

    🐛 fix: Solve query error

--- src/main/java/br/edu/ifba/conectairece/api/features/step/domain/repository/IStepRepository.java
+++ src/main/java/br/edu/ifba/conectairece/api/features/step/domain/repository/IStepRepository.java
@@ -3,11 +3,15 @@ package br.edu.ifba.conectairece.api.features.step.domain.repository;
 import br.edu.ifba.conectairece.api.features.step.domain.model.Step;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author Giovane Neves
+ */
 @Repository
 public interface IStepRepository extends JpaRepository<Step, UUID> {
 
@@ -18,7 +22,7 @@ public interface IStepRepository extends JpaRepository<Step, UUID> {
         WHERE fs.flow.id = :flowId
         ORDER BY fs.stepOrder ASC
     """)
-    List<Step> findAllByFlowId(UUID id);
+    List<Step> findAllByFlowId(@Param("flowId") UUID flowId);
 
 
 }
